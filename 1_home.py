6f445b7de45c9f979339a1eb271419e994f578df
User <user@MacBook-Pro-de-User.local>
2024-06-06 13:54:03 -0300

    first commit

---
+++ 1_home.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+import webbrowser
+from datetime import datetime
+
+if "data" not in st.session_state:
+    df_data = pd.read_csv("datasets/CLEAN_FIFA23_official_data.csv", index_col=0)
+    df_data = df_data[df_data["Contract Valid Until"] >= datetime.today().year]
+    df_data = df_data[df_data["Value(£)"] > 0]
+    df_data = df_data.sort_values(by="Overall", ascending=False)
+    st.session_state['data'] = df_data
+
+st.write("# FIFA OFICIAL DATASET")
+st.sidebar.write("Desenvolvido por: Ricardo Ferrari")
+
+btn = st.button("Acesse os dados no Kuggle")
+if btn:
+    webbrowser.open_new_tab("https://digital.gestionafacil.com.br/")
+
+st.markdown(
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+
+)
---
+++ pages/3_teams.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.write("Tabela completa com os dados dos jogadores.")
+st.session_state['data']
